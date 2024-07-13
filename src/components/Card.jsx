401f4069178aed711f1805dbbc84ad455a506be5
Ghaieth Grami <ghaieth.grami06@gmail.com>
2024-07-13 15:09:58 +0200

    card and data finished

---
+++ src/components/Card.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import swords from '../Images/swords.png';
+import dot from '../Images/dot.png';
+
+const Card = ({ img, title }) => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const OpenMenu = () => {
+    setShowMenu(!showMenu);
+  };
+  const CloseMenu = () => {
+    setShowMenu(false);
+  };
+  return (
+    <div>
+      <div>
+        <img src={img} alt='' />
+
+        <div>
+          <h1>{title}</h1>
+          <span>Premium</span>
+          <p>Recreate this wallpaper using AI.</p>
+          <button>
+            <img src={swords} alt='swords' />
+            Start this challenge
+          </button>
+          <div>
+            <button onClick={OpenMenu}>
+              <img src={dot} alt='dot' />
+            </button>
+            {showMenu && <Menu />}
+          </div>
+        </div>
+      </div>
+      {showMenu && <div onClick={CloseMenu}></div>}
+    </div>
+  );
+};
+
+export default Card;
